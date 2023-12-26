c7f808dd95a70c400cf5e2b883c329c948d5e96b
aslıhan yoldaş <aslihanyoldas24@gmail.com>
2023-12-26 16:00:33 +0300

    Deleted fuctions about popular posts' whole text and the csv file

--- module.py
+++ module.py
@@ -26,36 +26,5 @@ def get_popular_reddit_posts(api_key):
     popular_posts_df = pd.DataFrame(popular_posts)
     return popular_posts_df
 
-def get_post_whole_text(api_key, post_id):
-
-    querystring = {"post_id":post_id}
-    headers = {
-        "X-RapidAPI-Key": api_key,
-        "X-RapidAPI-Host": constants.RAPID_API_HOST
-    }
-
-    response = requests.get(constants.BASE_URL+ constants.POST_DETAILS_END_POINT, headers=headers, params=querystring)
-
-    post_detail = response.json()
-    post_text=post_detail['data']['media']['richtextContent']['document']
-    post_string=""
-    for line in post_text:
-        try:
-            post_string+=line['c'][0]['t']
-        except KeyError:
-            for nested_line in line['c'][0]['c']:
-                try:
-                    post_string+= nested_line['t']
-                except KeyError:
-                    post_string+=nested_line['c'][0]['t']
-                    continue
-            continue
-    return post_string
-
-def get_popular_reddit_posts_with_whole_text(api_key):
-    popular_posts = get_popular_reddit_posts(api_key)
-    post_texts = [get_post_whole_text(api_key=api_key, post_id=id) for id in popular_posts['id']]
-    popular_posts['post_text'] =post_texts
-    return popular_posts
 
    
